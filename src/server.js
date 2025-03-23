c08bf91d4602bc936ccfdf5d0fbf8ff26126f264
Marco Antonio Guerrero Asis <marcogasis01@gmail.com>
2025-03-23 02:25:20 -0600

    Se agrego cors para las peeticiones http

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { alumnoRoutes } from '../routes/alumnoRoutes.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(cors());
 
 
 
