97de8882f2d45d54ed69c54a01395a76eaedb62d
Marco Antonio Guerrero Asis <marcogasis01@gmail.com>
2025-03-24 02:18:17 -0600

    se agrego DB_DATABASE en db.js

--- src/db.js
+++ src/db.js
@@ -12,7 +12,7 @@ dotenv.config();
 const client = new Client({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: "proyectoqrDB",
+    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 });
